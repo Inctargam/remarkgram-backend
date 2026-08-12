d646db39e3696f2a777f936c538b2b5ffc1edd22
Viacheslav Solovev <slvsl.spb@gmail.com>
2026-08-13 00:39:55 +0300

    refactor: run grpc services as standalone microservices

--- apps/files/src/main.ts
+++ apps/files/src/main.ts
@@ -1,25 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import type { ConfigType } from '@nestjs/config';
-import type { MicroserviceOptions } from '@nestjs/microservices';
+import type { AsyncMicroserviceOptions, MicroserviceOptions } from '@nestjs/microservices';
 import { Transport } from '@nestjs/microservices';
 import { filesConfig } from './config/files.config.js';
 import { FilesModule } from './files.module.js';
 import { FILES_GRPC_PROTO_PATH, REMARKGRAM_FILES_V1_PACKAGE_NAME } from '@app/files-grpc';
 
 async function bootstrap() {
-  const app = await NestFactory.create(FilesModule);
+  const app = await NestFactory.createMicroservice<AsyncMicroserviceOptions>(FilesModule, {
+    inject: [filesConfig.KEY],
+    useFactory: (config: ConfigType<typeof filesConfig>): MicroserviceOptions => ({
+      transport: Transport.GRPC,
+      options: {
+        package: REMARKGRAM_FILES_V1_PACKAGE_NAME,
+        protoPath: FILES_GRPC_PROTO_PATH,
+        url: config.url,
+      },
+    }),
+  });
   const config = app.get<ConfigType<typeof filesConfig>>(filesConfig.KEY);
 
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.GRPC,
-    options: {
-      package: REMARKGRAM_FILES_V1_PACKAGE_NAME,
-      protoPath: FILES_GRPC_PROTO_PATH,
-      url: config.url,
-    },
-  });
-  await app.init();
-  await app.startAllMicroservices();
+  await app.listen();
   console.log('Server FILES started on port', config.url);
 }
 void bootstrap();
--- apps/posts/src/main.ts
+++ apps/posts/src/main.ts
@@ -1,24 +1,25 @@
 import { POSTS_GRPC_PROTO_PATH, REMARKGRAM_POSTS_V1_PACKAGE_NAME } from '@app/posts-grpc';
 import type { ConfigType } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import { Transport, type MicroserviceOptions } from '@nestjs/microservices';
+import { Transport, type AsyncMicroserviceOptions, type MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module.js';
 import { postsConfig } from './config/posts.config.js';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<AsyncMicroserviceOptions>(AppModule, {
+    inject: [postsConfig.KEY],
+    useFactory: (config: ConfigType<typeof postsConfig>): MicroserviceOptions => ({
+      transport: Transport.GRPC,
+      options: {
+        package: REMARKGRAM_POSTS_V1_PACKAGE_NAME,
+        protoPath: POSTS_GRPC_PROTO_PATH,
+        url: config.url,
+      },
+    }),
+  });
   const config = app.get<ConfigType<typeof postsConfig>>(postsConfig.KEY);
 
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.GRPC,
-    options: {
-      package: REMARKGRAM_POSTS_V1_PACKAGE_NAME,
-      protoPath: POSTS_GRPC_PROTO_PATH,
-      url: config.url,
-    },
-  });
-  await app.init();
-  await app.startAllMicroservices();
+  await app.listen();
   console.log('Server POSTS started on port', config.url);
 }
 void bootstrap();
